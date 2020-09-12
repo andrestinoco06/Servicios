f1372d59fd17f121dc6538eaadb3b15b5912f2ea
johan <johan@DESKTOP-M7ILL3F>
2020-09-12 16:36:06 -0500

    Paquetes

---
+++ Servicios/Servicios-web/src/main/java/co/edu/unicundi/controller/UsuarioController.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicundi.controller;
+
+import co.edu.unicundi.logica.ServiceProfesor;
+import co.edu.unicundi.pojo.Usuario;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author johan
+ */
+@Stateless
+@Path("/usuario")
+public class UsuarioController{
+
+    @Inject
+    private ServiceProfesor serviceProfesor;
+    
+    /*
+        Crea un profesor
+    */
+    @Path("/crearProfesor")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response crearUsuario(Usuario usuario) {
+        return serviceProfesor.crearProfesor(usuario);
+    }
+
+    /*
+     Retorna todos los profesores
+     */
+    @Path("/todosProfesores")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response todosProfesores() {
+        System.out.println("entro al servicio");
+        return serviceProfesor.todosProfesores();
+    }
+
+    /*
+     Edita a los profesores
+     */
+    @Path("/editarProfesor")
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response editarProfesor(Usuario usuario) {
+        return serviceProfesor.editarProfesor(usuario);
+    }
+
+    /*
+     Elimina a los profesores por id
+     */
+    @Path("/eliminarProfesor/{numero}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response eliminarProfesor(@PathParam("numero") int numero) {
+        return serviceProfesor.eliminarProfesor(numero);
+        
+    }
+
+    /*
+     Busca por cedula al profesor
+     */
+    @Path("/buscaCedula/{numero}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response buscarCedulaProfesor(@PathParam("numero") String numero) {
+        return serviceProfesor.buscarProfesor(numero);
+    }
+
+    /*
+     Busca a los profesores que corresponden a las materias 
+     */
+    @Path("/buscaMaterias/{nombre}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response buscarMaterias(@PathParam("nombre") String nombre) {
+        return serviceProfesor.buscarMaterias(nombre);
+    }
+
+}
---
+++ Servicios/Servicios-web/src/main/java/co/edu/unicundi/logica/ServiceProfesor.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicundi.logica;
+
+import co.edu.unicundi.lecturaEscritura.lecturaEscristuraProfesor;
+import co.edu.unicundi.pojo.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author johan
+ */
+@Named(value = "ServiceProfesor")
+@RequestScoped
+public class ServiceProfesor {
+
+    public Response crearProfesor(Usuario usuario) {
+        List<Usuario> lista = new lecturaEscristuraProfesor().verProfesor();
+        boolean validacion = true;
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getCedula().equals(usuario.getCedula())) {
+                System.out.println("ENTRO AL IF ");
+                validacion = false;
+            }
+        }
+        if (validacion == true) {
+            lista.add(usuario);
+            new lecturaEscristuraProfesor().crearArchivo(lista);
+            return Response.status(Response.Status.CREATED).build();
+        }else{
+            return Response.status(Response.Status.EXPECTATION_FAILED).entity("El numero de cedula ya se encuentra registrado").build();
+        }
+    }
+    
+    public Response todosProfesores() {
+        List<Usuario> lista = new lecturaEscristuraProfesor().verProfesor();
+        if (lista.size() != 0) {
+            return Response.status(Response.Status.OK).entity(lista).build();
+        }else{
+            return Response.status(Response.Status.NOT_ACCEPTABLE).entity("No hay profesores registrados").build();
+        }
+    }
+
+    public Response editarProfesor(Usuario usuario){
+        List<Usuario> lista = new lecturaEscristuraProfesor().verProfesor();
+        boolean validacion = false;
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getId() == usuario.getId()) {
+                lista.get(i).setNombre(usuario.getNombre());
+                lista.get(i).setApellido(usuario.getApellido());
+                lista.get(i).setMaterias(usuario.getMaterias());
+                lista.get(i).setCedula(usuario.getCedula());
+                lista.get(i).setCorreo(usuario.getCorreo());
+                validacion = true;
+            }
+        }
+        if(validacion==true){
+            new lecturaEscristuraProfesor().crearArchivo(lista);
+            return Response.status(Response.Status.OK).entity("Se ha modificado con éxito").build();
+        }else{
+            return Response.status(Response.Status.NOT_MODIFIED).entity("No se encontro el profesor").build();
+        }
+    }
+    
+    public Response eliminarProfesor(int id){
+        List<Usuario> lista = new lecturaEscristuraProfesor().verProfesor();
+        boolean estado = false;
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getId() == id) {
+                lista.remove(i);
+                estado = true;
+            }
+        }
+        
+        if(estado == true){
+            new lecturaEscristuraProfesor().crearArchivo(lista);
+            return Response.status(Response.Status.OK).entity("Se elimino correctamente").build();
+        }else{
+            return Response.status(Response.Status.EXPECTATION_FAILED).entity("No se encontro el profesor").build();
+        }
+    }
+    
+    public Response buscarProfesor(String numero){
+        List<Usuario> lista = new lecturaEscristuraProfesor().verProfesor();
+        Usuario datos = new Usuario();
+        boolean estado = false;
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getCedula().equals(numero)) {
+                datos = lista.get(i);
+                estado = true;
+            }
+        }
+        if (estado == true) {
+            return Response.status(Response.Status.OK).entity(datos).build();
+        } else {
+            return Response.status(Response.Status.EXPECTATION_FAILED).entity("No se encontro ningún profesor").build();
+        }
+    }
+    
+    public Response buscarMaterias(String nombre){
+        List<Usuario> lista = new lecturaEscristuraProfesor().verProfesor();
+        List<Usuario> busqueda = new ArrayList<>();
+        boolean estado = false;
+        for (int i = 0; i < lista.size(); i++) {
+            List<String> materias = lista.get(i).getMaterias();
+            for (int x = 0; x < materias.size(); x++) {
+                if (materias.get(x).equals(nombre)) {
+                    busqueda.add(lista.get(i));
+                    estado = true;
+                }
+            }
+        }
+        if (estado == true) {
+            return Response.status(Response.Status.OK).entity(busqueda).build();
+        } else {
+            return Response.status(Response.Status.EXPECTATION_FAILED).entity("No se encontro profesores").build();
+        }
+    }
+}
